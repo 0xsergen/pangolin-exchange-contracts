48f2d8c9d2c14df4ab19e0a23f96befff2d7b1c5
Sergio <sergio2098crypto@gmail.com>
2023-04-04 11:18:06 +0300

    updating zksync packages and deploying contracts again

--- zksync-hardhat.config.js
+++ zksync-hardhat.config.js
@@ -3,33 +3,35 @@ require("@matterlabs/hardhat-zksync-solc");
 require("@matterlabs/hardhat-zksync-verify");
 require("dotenv").config();
 
-var exports = require('./hardhat.config.js');
+var exports = require("./hardhat.config.js");
 const soliditySettings = exports.solidity;
 
-const { TASK_COMPILE_SOLIDITY_GET_SOURCE_PATHS } = require("hardhat/builtin-tasks/task-names");
+const {
+  TASK_COMPILE_SOLIDITY_GET_SOURCE_PATHS,
+} = require("hardhat/builtin-tasks/task-names");
 const path = require("path");
 
-const ignoreList = [
-  "MiniChefV2Zapper.sol",
-]
+const ignoreList = ["MiniChefV2Zapper.sol"];
 
 subtask(
   TASK_COMPILE_SOLIDITY_GET_SOURCE_PATHS,
   async (_, { config }, runSuper) => {
     const paths = await runSuper();
 
-    return paths
-      .filter(solidityFilePath => {
-        const relativePath = path.relative(config.paths.sources, solidityFilePath)
-        const fileName = path.basename(relativePath);
-        return !ignoreList.includes(fileName);
-      })
+    return paths.filter((solidityFilePath) => {
+      const relativePath = path.relative(
+        config.paths.sources,
+        solidityFilePath
+      );
+      const fileName = path.basename(relativePath);
+      return !ignoreList.includes(fileName);
+    });
   }
 );
 
 module.exports = {
   zksolc: {
-    version: "1.3.1",
+    version: "1.3.4",
     compilerSource: "binary",
     settings: {
       isSystem: true,
@@ -38,12 +40,13 @@ module.exports = {
   defaultNetwork: "zksync_testnet_goerli",
   solidity: soliditySettings,
   networks: {
-    "zksync_testnet_goerli": {
+    zksync_testnet_goerli: {
       accounts: [process.env.PRIVATE_KEY],
       url: "https://zksync2-testnet.zksync.dev",
       ethNetwork: "goerli",
       zksync: true,
-      verifyURL: 'https://zksync2-testnet-explorer.zksync.dev/contract_verification'
+      verifyURL:
+        "https://zksync2-testnet-explorer.zksync.dev/contract_verification",
     },
   },
 };
